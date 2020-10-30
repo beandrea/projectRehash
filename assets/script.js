70a58196918bd7d17dc1c8f84650fca2659adf84
Izzy <65364798+queenIsabella99@users.noreply.github.com>
2020-10-29 21:26:03 -0400

    starting to work

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,25 @@
-var apiKey = "1c6fdd4cb29d4a66960f79e845689e9d";
-var querylURL = "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=test&q_track=God's%20Plan&q_artist=Drake&apikey=" + apiKey
-
-function test (data) {
-    console.log(data);
-}
-
-var scr = $("<script>").attr("src", querylURL);
-$("body").append(scr);
-
 var apiKey2 = "00eed5848e963c43bd2cd73c8707c667";
 var querylURL2 = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=" + apiKey2 + "&format=json";
+
+function test(data) {
+    $("#lyric").text(data.message.body.lyrics.lyrics_body);
+}
 
 $.ajax({
     url: querylURL2,
     method: "GET"
 }).then(function (response) {
     console.log(response);
+});
+
+$("#search").on("click", function () {
+    var artist = $("#artist").val();
+    var song = $("#song").val();
+
+    var apiKey = "1c6fdd4cb29d4a66960f79e845689e9d";
+    var querylURL = "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=jsonp&callback=test&q_track=" + 
+        song + "&q_artist=" + artist + "&apikey=" + apiKey;
+
+    var scr = $("<script>").attr("src", querylURL);
+    $("body").append(scr);
 });
